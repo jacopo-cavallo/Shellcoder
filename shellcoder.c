9bfb4ffd77560715c6e2e97dc667874765e5c9d5
Jacopo Cavallo <jacopo.cavallo@protonmail.com>
2016-12-14 04:09:49 +0100

    Update shellcoder.c

--- shellcoder.c
+++ shellcoder.c
@@ -57,9 +57,17 @@ down:
 int main(int argc, char **argv)
 {
 	int i;
-	size_t len = strlen(argv[1]);
+	size_t len;
 	unsigned char shellcode[LEN_SKELETON + len*4 + 1];
 	unsigned char program[len*4+1];
+	
+	if(argc < 2)
+	{
+		printf("Usage:%s </path/to/bin>\n",argv[0]);
+		return 1;
+	}
+	
+	len = strlen(argv[1]);
 	
 	snprintf(shellcode, LEN_SKELETON, "\\x48\\x31\\xc0\\x48\\x31\\xff\\x48\\x31\\xf6\\x48"
 					  "\\x31\\xd2\\xb0\\x71\\x0f\\x05\\x48\\x31\\xc0\\xeb"
